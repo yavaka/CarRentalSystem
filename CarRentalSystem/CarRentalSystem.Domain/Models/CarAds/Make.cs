13ca05e28b5d722569a1cd412600fec6b643103c
yavaka <yavakaa@gmail.com>
2020-06-10 09:57:24 +0100

    Add Make/Manufacturer aggregate child entity.

--- CarRentalSystem/CarRentalSystem.Domain/Models/CarAds/Make.cs
+++ CarRentalSystem/CarRentalSystem.Domain/Models/CarAds/Make.cs
@@ -1,8 +1,31 @@
 ﻿using CarRentalSystem.Domain.Common;
+using CarRentalSystem.Domain.Exceptions;
+using static CarRentalSystem.Domain.Models.ValidationConstants.Common;
 
 namespace CarRentalSystem.Domain.Models.CarAds
 {
     public class Make : Entity<int>
     {
+        //DDD rule: only aggregate root entities` constructors should be public
+        //DDD rule: the constructor should validate object/s
+        internal Make(string name) 
+        {
+            this.Validate(name);
+
+            this.Name = name;
+        }
+
+
+        public string Name { get; }
+
+
+        private void Validate(string name)
+        {
+            Guard.ForStringLength<InvalidCarAdException>(
+                name,
+                MinNameLength,
+                MaxNameLength,
+                nameof(name));
+        }
     }
 }
