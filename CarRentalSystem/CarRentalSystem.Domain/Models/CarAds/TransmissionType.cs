8d6ce4556074c90c210ac08eba41800081219307
yavaka <yavakaa@gmail.com>
2020-06-10 10:21:19 +0100

    Add TransmissionType enumeration class.

--- CarRentalSystem/CarRentalSystem.Domain/Models/CarAds/TransmissionType.cs
+++ CarRentalSystem/CarRentalSystem.Domain/Models/CarAds/TransmissionType.cs
@@ -1,6 +1,14 @@
-﻿namespace CarRentalSystem.Domain.Models.CarAds
+﻿using CarRentalSystem.Domain.Common;
+
+namespace CarRentalSystem.Domain.Models.CarAds
 {
-    public class TransmissionType
+    public class TransmissionType : Enumeration
     {
+        public static readonly TransmissionType Manual = new TransmissionType(1, nameof(Manual));
+
+        public static readonly TransmissionType Automatic = new TransmissionType(1, nameof(Automatic));
+
+        public TransmissionType(int value, string name)
+            : base(value, name) { }
     }
 }
