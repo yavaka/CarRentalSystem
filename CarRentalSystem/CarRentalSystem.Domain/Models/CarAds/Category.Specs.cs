1f9ce2ce086c2b9ed609b55d6bfe76c3ef93ee13
yavaka <yavakaa@gmail.com>
2020-06-10 11:25:16 +0100

    Add Category unit tests.

---
+++ CarRentalSystem/CarRentalSystem.Domain/Models/CarAds/Category.Specs.cs
@@ -0,0 +1,47 @@
+﻿using CarRentalSystem.Domain.Exceptions;
+using FluentAssertions;
+using System;
+using System.Security.Cryptography;
+using Xunit;
+
+namespace CarRentalSystem.Domain.Models.CarAds
+{
+    public class CategorySpecs
+    {
+        [Fact]
+        public void ValidCategoryShouldNotThrowException()
+        {
+            //Act
+            Action act = () => new Category(
+                name: "Saloon",
+                description: "Saloon, A.K.A sedans, has longer wheelbase than conventional family hatchback.");
+
+            //Assert
+            act.Should().NotThrow<InvalidCarAdException>();
+        }
+
+        [Fact]
+        public void InvalidNameShouldThrowException()
+        {
+            //Act
+            Action act = () => new Category(
+                name: "",
+                description: "It is a valid description.");
+
+            //Assert
+            act.Should().Throw<InvalidCarAdException>();
+        }
+
+        [Fact]
+        public void InvalidDescriptionShouldThrowException()
+        {
+            //Act
+            Action act = () => new Category(
+                name: "Valid name.",
+                description: "");
+
+            //Assert
+            act.Should().Throw<InvalidCarAdException>();
+        }
+    }
+}
