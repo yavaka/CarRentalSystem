8cc74a7fd4da25dcf725c3d2a78f0ac864e45774
yavaka <yavakaa@gmail.com>
2020-06-14 12:15:55 +0100

    Add Dealer and PhoneNumber unit tests.

---
+++ CarRentalSystem/CarRentalSystem.Domain/Models/Dealers/Dealer.Specs.cs
@@ -0,0 +1,43 @@
+﻿using CarRentalSystem.Domain.Exceptions;
+using CarRentalSystem.Domain.Models.CarAds;
+using FakeItEasy;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CarRentalSystem.Domain.Models.Dealers
+{
+    public class DealerSpecs
+    {
+        [Fact]
+        public void ValidDealerShouldSaveCarAd() 
+        {
+            //Arrange
+            var dealer = new Dealer(
+                name:"Private dealer",
+                phoneNumber: "+123456789");
+
+            //Instantiate a new CarAd entity from Fake Factory
+            var carAd = A.Dummy<CarAd>();
+
+            //Act
+            dealer.AddCarAd(carAd);
+
+            //Assert
+            dealer.CarAds.Should()
+                .Contain(carAd);
+        }
+
+        [Fact]
+        public void InvalidDealerShouldThrowException()
+        {
+            //Act
+            Action act = () => new Dealer(
+                name: "",
+                phoneNumber: "+123456789");
+
+            //Assert
+            act.Should().Throw<InvalidDealerException>();
+        }
+    }
+}
---
+++ CarRentalSystem/CarRentalSystem.Domain/Models/Dealers/PhoneNumber.Specs.cs
@@ -0,0 +1,42 @@
+﻿using CarRentalSystem.Domain.Exceptions;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CarRentalSystem.Domain.Models.Dealers
+{
+    public class PhoneNumberSpecs
+    {
+        [Fact]
+        public void ValidPhoneNumberShouldNotThrowException() 
+        {
+            //Act
+            Action act = () => new PhoneNumber("+123456789");
+
+            //Assert
+            act.Should().NotThrow<InvalidPhoneNumberException>();
+        }
+
+        [Fact]
+        public void InvalidPhoneNumberFirstSymbolShouldThrowException() 
+        {
+            //Act
+            Action act = () => new PhoneNumber("*12455647773");
+
+            //Assert
+            act.Should().Throw<InvalidPhoneNumberException>();
+        }
+
+        [Fact]
+        public void InvalidPhoneNumberLengthShouldThrowException() 
+        {
+            //Act
+            Action act = () => new PhoneNumber("");
+
+            //Assert
+            act.Should().Throw<InvalidPhoneNumberException>();
+        }
+    }
+}
