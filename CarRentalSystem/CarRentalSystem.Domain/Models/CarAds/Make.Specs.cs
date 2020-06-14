77883edd161a96b153db6c6394991a22ccef1cf0
yavaka <yavakaa@gmail.com>
2020-06-14 11:13:33 +0100

    Add Make/Manufacturer unit tests.

---
+++ CarRentalSystem/CarRentalSystem.Domain/Models/CarAds/Make.Specs.cs
@@ -0,0 +1,30 @@
+﻿using CarRentalSystem.Domain.Exceptions;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CarRentalSystem.Domain.Models.CarAds
+{
+    public class MakeSpecs
+    {
+        [Fact]
+        public void ValidMakeShouldNotThrowException() 
+        {
+            //Act
+            Action act = () => new Make(name: "Subaru");
+
+            //Assert
+            act.Should().NotThrow<InvalidCarAdException>();
+        }
+
+        [Fact]
+        public void InvalidMakeShouldThrowException()
+        {
+            //Act
+            Action act = () => new Make(name: "");
+
+            //Assert
+            act.Should().Throw<InvalidCarAdException>();
+        }
+    }
+}
