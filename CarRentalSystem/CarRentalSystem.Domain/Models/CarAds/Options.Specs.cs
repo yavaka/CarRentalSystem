fb2223b50ccd2ee8189979cf700d0604e0858ee3
yavaka <yavakaa@gmail.com>
2020-06-14 11:23:34 +0100

    Add Options unit tests.

---
+++ CarRentalSystem/CarRentalSystem.Domain/Models/CarAds/Options.Specs.cs
@@ -0,0 +1,34 @@
+﻿using CarRentalSystem.Domain.Exceptions;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CarRentalSystem.Domain.Models.CarAds
+{
+    public class OptionsSpecs
+    {
+        [Fact]
+        public void ValidOptionsShouldNotThrowException() 
+        {
+            //Act
+            Action act = () => new Options(
+                hasClimateControl: true,
+                seats: 5,
+                transmissionType: TransmissionType.Manual);
+
+            act.Should().NotThrow<InvalidOptionsException>();
+        }
+
+        [Fact]
+        public void InvalidOptionsShouldThrowException()
+        {
+            //Act
+            Action act = () => new Options(
+                hasClimateControl: true,
+                seats: 22,
+                transmissionType: TransmissionType.Manual);
+
+            act.Should().Throw<InvalidOptionsException>();
+        }
+    }
+}
