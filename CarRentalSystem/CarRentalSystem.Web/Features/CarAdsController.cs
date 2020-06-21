a2eded9a1eec367fadb9c5720f32e1d633e3e2a9
yavaka <yavakaa@gmail.com>
2020-06-21 19:13:21 +0100

    Add CarAd controller

---
+++ CarRentalSystem/CarRentalSystem.Web/Features/CarAdsController.cs
@@ -0,0 +1,26 @@
+﻿using CarRentalSystem.Domain.Models.CarAds;
+using CarRentalSystem.Domain.Models.Dealers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarRentalSystem.Web.Features
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CarAdsController : ControllerBase
+    {
+        private static readonly Dealer _dealer = new Dealer(
+            name: "Test dealer",
+            phoneNumber: "+12342355436");
+
+        [HttpGet]
+        //Get car ads of the given dealer
+        public IEnumerable<CarAd> Get() 
+        {
+            return _dealer.CarAds
+                .Where(a =>a.IsAvailable);
+        }
+    }
+}
