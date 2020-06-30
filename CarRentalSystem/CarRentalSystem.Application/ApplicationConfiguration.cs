f05104ec0f9c3fde51ddfcb7d5649bfa9e1b9029
yavaka <yavakaa@gmail.com>
2020-06-30 10:40:49 +0100

    Add and configure MediatR in Application layer.

--- CarRentalSystem/CarRentalSystem.Application/ApplicationConfiguration.cs
+++ CarRentalSystem/CarRentalSystem.Application/ApplicationConfiguration.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace CarRentalSystem.Application
 {
@@ -10,7 +12,9 @@ namespace CarRentalSystem.Application
         {
             return services.Configure<ApplicationSettings>(
                 configuration.GetSection(nameof(ApplicationSettings)),
-                options => options.BindNonPublicProperties = true);
+                options => options.BindNonPublicProperties = true)
+                //configures MediatR
+                .AddMediatR(Assembly.GetExecutingAssembly());
         }
     }
 }
